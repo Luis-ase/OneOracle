244aff9abdc4a78fcebfb0cf898b7a4cf5833b9f
Michael Luis Condori Paye <luis2003nb@gmail.com>
2023-06-12 19:46:54 -0300

    Java One oracle 01

---
+++ src/Poo/CreateCuenta.java
@@ -0,0 +1,32 @@
+package Poo;
+
+public class CreateCuenta {
+    public static void main(String[] args) {
+        // instanciar una cuenta
+        check primeraCuenta = new check();
+        // forma de ingresar valor a la cuenta 
+        primeraCuenta.saldo = 100;
+        check segunda = new check();
+        segunda.saldo = 100;
+        /*
+         * new check = {
+         *  
+         *  double saldo;
+            int agencia;
+            int numero;
+            String titular;
+                    key
+         * }
+         * instancia: 
+         * primeraCuenta es 
+         * check = {
+         *  saldo   =   100
+         *  agencia =   2
+         *  numero  =   3
+         *  titular =   Survi
+         *  key         value
+         * }
+         * 
+         */
+    }
+}
