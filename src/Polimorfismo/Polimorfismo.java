4f2fcb440916dae8c774faee36c58acc39b1dfc7
Michael Luis Condori Paye <luis2003nb@gmail.com>
2023-06-14 17:16:00 -0300

    JAVA OneOracle 03

--- src/Polimorfismo/Polimorfismo.java
+++ src/Polimorfismo/Polimorfismo.java
@@ -3,10 +3,15 @@ package Polimorfismo;
 public class Polimorfismo {
     public static void main(String[] args) {
         Funcionario capo = new Funcionario();
-        capo.setNombre("capito");
-        capo.setSalario(2000);
-        System.out.println(capo.getBonificacion(0.1));
-        
-        System.out.println(capo.getNombre().toString());
+        try {
+            capo.setNombre("capito");
+            capo.setSalario(2000);
+            System.out.println(capo.getBonificacion(0.1));
+            
+            System.out.println(capo.getNombre().toString());
+            System.out.println("No salgo");
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
     }
 }
